5f7bda33420d412dd7e9158c91f57203221004b6
NecoSama <598507680@qq.com>
2016-06-28 13:06:33 +0800

    fs服务端一键安装脚本

--- install_fs.sh
+++ install_fs.sh
@@ -4,7 +4,7 @@ export PATH
 
 
 install_path=/fs/
-package_download_url=https://github.com/PengQianheng/FinalSpeed/blob/master/finalspeed_server.zip
+package_download_url=https://raw.githubusercontent.com/PengQianheng/FinalSpeed/master/finalspeed_server.zip
 package_save_name=finalspeed_server.zip
  
 function checkjava(){
